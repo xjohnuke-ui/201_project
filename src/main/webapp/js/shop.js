b14b9afeadc61198cecb82b0a5cd0ca830e08ebc
CHENGZIANG1031 <cza3199844086@gmail.com>
2026-01-08 00:23:00 +0800

    feat(cart): implement cart and order backend

---
+++ src/main/webapp/js/shop.js
@@ -0,0 +1,21 @@
+console.log("shop.js loaded");
+
+document.addEventListener('DOMContentLoaded', function () {
+    const categoryItems = document.querySelectorAll('.category-item');
+    categoryItems.forEach(item => {
+        item.addEventListener('click', function (event) {
+            // 移除所有 li 的 active 类
+            categoryItems.forEach(item => {
+                item.classList.remove('active');
+                const link=item.querySelector('a');
+                link.style.color='';
+            });
+            // 给当前点击的 li 添加 active 类
+            this.classList.add('active');
+            const link=this.querySelector('a');
+            link.style.color='#ff0036';
+            event.stopPropagation();
+            // // event.preventDefault();
+        });
+    });
+})
